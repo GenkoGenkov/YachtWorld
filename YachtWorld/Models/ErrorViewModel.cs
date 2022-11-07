2d59ea86ac49bde62d3519a9c2036cf04b467107
GenkoGenkov <werto.haho@gmail.com>
2022-11-07 14:31:05 +0200

    Add project files.

---
+++ YachtWorld/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace YachtWorld.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
