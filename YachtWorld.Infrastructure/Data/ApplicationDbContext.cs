6b7d890cdb386e87282615b03b9e8abb9ae56473
GenkoGenkov <werto.haho@gmail.com>
2022-11-07 17:31:53 +0200

    Moving DB Context and Migrations to another project

--- YachtWorld.Infrastructure/Data/ApplicationDbContext.cs
+++ YachtWorld.Infrastructure/Data/ApplicationDbContext.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
-namespace YachtWorld.Data
+namespace YachtWorld.Infrastructure.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
