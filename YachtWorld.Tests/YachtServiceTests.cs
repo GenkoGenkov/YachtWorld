03485412260794eeae3d160faf657cf687f7403f
GenkoGenkov <werto.haho@gmail.com>
2022-12-12 18:30:00 +0200

    Adding tests for YachtService

--- YachtWorld.Tests/YachtServiceTests.cs
+++ YachtWorld.Tests/YachtServiceTests.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using Moq;
+using System.Linq;
 using YachtWorld.Core.Contracts;
 using YachtWorld.Core.Exceptions;
 using YachtWorld.Core.Models.Yacht;
@@ -32,6 +33,187 @@ namespace YachtWorld.Tests
 
             applicationDbContext.Database.EnsureDeleted();
             applicationDbContext.Database.EnsureCreated();
+        }
+
+        //[Test]
+        //public async Task TestCreateShouldCreateYacht()
+        //{
+        //    var loggerMock = new Mock<ILogger<YachtService>>();
+        //    logger = loggerMock.Object;
+
+        //    var repo = new Repository(applicationDbContext);
+
+        //    yachtService = new YachtService(repo, guard, logger);
+
+        //    var initialYachts = repo.All<Yacht>().Count();
+
+        //    YachtModel newYacht = new Yacht() 
+        //    {
+        //        Id = 1,
+        //        CategoryId = 2,
+        //        ShipyardId = 1,
+        //        DestinationId = 1,
+        //        Description = "",
+        //        ImageUrl = "",
+        //        PriceForRent = 180_000,
+        //        Title = ""
+
+        //    };
+
+        //    var newYachtId = await yachtService.Create(newYacht, 1);
+
+        //    var yachtCountAfterNewOne = repo.All<Yacht>().Count();
+
+        //    Assert.That(yachtCountAfterNewOne, Is.EqualTo(initialYachts + 1));
+        //}
+
+        [Test]
+        public async Task TestYachtDetailsByIdShouldReturnCorrectYachtData()
+        {
+            var loggerMock = new Mock<ILogger<YachtService>>();
+            logger = loggerMock.Object;
+
+            var repo = new Repository(applicationDbContext);
+
+            yachtService = new YachtService(repo, guard, logger);
+
+            var yachtId = repo.All<Yacht>().Select(y => y.SailorId != null).FirstOrDefault().ToString();
+            int intYachtId = yachtId == null ? 0 : 1;
+            var result = await yachtService.YachtDetailsById(intYachtId);
+            var yachtsInDb = applicationDbContext.Yachts.Find(intYachtId);
+
+            Assert.IsNotNull(result);
+            Assert.That(result.Id, Is.EqualTo(yachtsInDb.Id));
+            Assert.That(result.Title, Is.EqualTo(yachtsInDb.Title));
+        }
+
+        [Test]
+        public async Task TestAllYachtsByUserIdShouldReturnCorrectYacht()
+        {
+            var loggerMock = new Mock<ILogger<YachtService>>();
+            logger = loggerMock.Object;
+
+            var repo = new Repository(applicationDbContext);
+
+            yachtService = new YachtService(repo, guard, logger);
+
+            var sailorId = repo.All<Yacht>().Select(y => y.SailorId).FirstOrDefault().ToString();
+            var result = await yachtService.AllYachtsByUserId(sailorId);
+            var yachtsInDb = repo.All<Yacht>().Where(y => y.SailorId.ToString() == sailorId);
+
+            Assert.IsNotNull(result);
+            Assert.That(result.Count(), Is.EqualTo(yachtsInDb.Count()));
+        }
+
+        [Test]
+        public async Task TestAllYachtsByYachtBrokerIdShouldReturnCorrectYacht()
+        {
+            var loggerMock = new Mock<ILogger<YachtService>>();
+            logger = loggerMock.Object;
+
+            var repo = new Repository(applicationDbContext);
+
+            yachtService = new YachtService(repo, guard, logger);
+
+            var yachtBrokerId = repo.All<YachtBroker>().FirstOrDefault().ToString();
+            var result = await yachtService.AllYachtsByUserId(yachtBrokerId);
+            var yachtsInDb = repo.All<YachtBroker>().Where(y => y.Id.ToString() == yachtBrokerId);
+
+            Assert.IsNotNull(result);
+            Assert.That(result.Count(), Is.EqualTo(yachtsInDb.Count()));
+        }
+
+        [Test]
+        public async Task TestAllCategoriesShouldReturnCorrectCategories()
+        {
+            var loggerMock = new Mock<ILogger<YachtService>>();
+            logger = loggerMock.Object;
+
+            var repo = new Repository(applicationDbContext);
+
+            yachtService = new YachtService(repo, guard, logger);
+
+            var result = await yachtService.AllCategories();
+            var dbCategories = repo.All<Category>();
+            var categoryNames = dbCategories.Select(c => c.Name);
+
+            Assert.That(result.Count(), Is.EqualTo(dbCategories.Count()));
+            Assert.That(categoryNames.Contains(result.FirstOrDefault().Name));
+        }
+
+        [Test]
+        public async Task TestAllShipyardsShouldReturnCorrectShipyard()
+        {
+            var loggerMock = new Mock<ILogger<YachtService>>();
+            logger = loggerMock.Object;
+
+            var repo = new Repository(applicationDbContext);
+
+            yachtService = new YachtService(repo, guard, logger);
+
+            var result = await yachtService.AllShipyards();
+            var dbCategories = repo.All<Shipyard>();
+            var categoryNames = dbCategories.Select(c => c.Name);
+
+            Assert.That(result.Count(), Is.EqualTo(dbCategories.Count()));
+            Assert.That(categoryNames.Contains(result.FirstOrDefault().Name));
+        }
+
+        [Test]
+        public async Task TestAllDestinationsShouldReturnCorrectDestinations()
+        {
+            var loggerMock = new Mock<ILogger<YachtService>>();
+            logger = loggerMock.Object;
+
+            var repo = new Repository(applicationDbContext);
+
+            yachtService = new YachtService(repo, guard, logger);
+
+            var result = await yachtService.AllDestinations();
+            var dbCategories = repo.All<Destination>();
+            var categoryNames = dbCategories.Select(c => c.Name);
+
+            Assert.That(result.Count(), Is.EqualTo(dbCategories.Count()));
+            Assert.That(categoryNames.Contains(result.FirstOrDefault().Name));
+        }
+
+        [Test]
+        public async Task TestAllCategorieNamesShouldReturnCorrectCategorieNames()
+        {
+            var loggerMock = new Mock<ILogger<YachtService>>();
+            logger = loggerMock.Object;
+
+            var repo = new Repository(applicationDbContext);
+
+            yachtService = new YachtService(repo, guard, logger);
+
+            var result = await yachtService.AllCategoriesNames();
+            var dbCategories = repo.All<Category>();
+            var categoryNames = dbCategories.Select(c => c.Name);
+
+            Assert.That(result.Count(), Is.EqualTo(dbCategories.Count()));
+        }
+
+        [Test]
+        public async Task TestAllYachtShouldReturnCorrectYachts()
+        {
+            var loggerMock = new Mock<ILogger<YachtService>>();
+            logger = loggerMock.Object;
+
+            var repo = new Repository(applicationDbContext);
+
+            yachtService = new YachtService(repo, guard, logger);
+
+            var searchTerm = "First";
+            var result = await yachtService.All(null, searchTerm);
+            var yachtsInDb = repo.All<Yacht>().Where(y => y.Title.Contains(searchTerm));
+            var resultYacht = result.Yachts.FirstOrDefault();
+            var yachtInDb = yachtsInDb.FirstOrDefault();
+
+            Assert.That(result.TotalYachtsCount, Is.EqualTo(yachtsInDb.Count()));
+            Assert.IsNotNull(result);
+            Assert.That(resultYacht?.Id, Is.EqualTo(yachtInDb?.Id));
+            Assert.That(resultYacht?.Title, Is.EqualTo(yachtInDb?.Title));
         }
 
         [Test]
