d01291cc54200a4e4c791c3009245e122b663bbf
GenkoGenkov <werto.haho@gmail.com>
2022-12-13 19:36:40 +0200

    Adding StatisticsService and RentService tests

---
+++ YachtWorld.Tests/RentServiceTests.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YachtWorld.Core.Contracts;
+using YachtWorld.Infrastructure.Data.Common;
+using YachtWorld.Infrastructure.Data;
+using YachtWorld.Core.Contracts.Admin;
+using YachtWorld.Core.Services;
+using YachtWorld.Core.Services.Admin;
+
+namespace YachtWorld.Tests
+{
+    [TestFixture]
+    public class RentServiceTests
+    {
+        private IRentService rentService;
+        private ApplicationDbContext applicationDbContext;
+
+        [SetUp]
+        public void Setup()
+        {
+            var contextOptions = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase("YachtDB")
+                .Options;
+
+            applicationDbContext = new ApplicationDbContext(contextOptions);
+
+            applicationDbContext.Database.EnsureDeleted();
+            applicationDbContext.Database.EnsureCreated();
+        }
+
+        [Test]
+        public async Task TestAllShouldReturnCorrectData()
+        {
+            rentService = new RentService(applicationDbContext);
+
+            var result = this.rentService.All();
+            var rentedYachtsInDb = applicationDbContext.Yachts.Where(y => y.SailorId != null);
+            //var resultYacht= result.ToList().Find(y => y.YachtTitle == )
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(rentedYachtsInDb.Count(), result.ToList().Count());
+        }
+    }
+}
---
+++ YachtWorld.Tests/StatisticsServiceTests.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YachtWorld.Core.Contracts;
+using YachtWorld.Infrastructure.Data.Common;
+using YachtWorld.Infrastructure.Data;
+using YachtWorld.Core.Services;
+
+namespace YachtWorld.Tests
+{
+    [TestFixture]
+    public class StatisticsServiceTests
+    {
+        private IRepository repo;
+        private IStatisticsService statisticsService;
+        private ApplicationDbContext applicationDbContext;
+
+        [SetUp]
+        public void Setup()
+        {
+            var contextOptions = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase("YachtDB")
+                .Options;
+
+            applicationDbContext = new ApplicationDbContext(contextOptions);
+
+            applicationDbContext.Database.EnsureDeleted();
+            applicationDbContext.Database.EnsureCreated();
+        }
+
+        [Test]
+        public async Task TestTotalShouldReturnCorrectCounts()
+        {
+            var repo = new Repository(applicationDbContext);
+
+            statisticsService = new StatisticsService(repo);
+
+            var result = await statisticsService.Total();
+            var yachtsCount = applicationDbContext.Yachts.Count();
+            var rentsCount = applicationDbContext.Yachts.Where(y => y.SailorId != null).Count();
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(yachtsCount, result.TotalYachts);
+            Assert.AreEqual(rentsCount, result.TotalRents);
+        }
+    }
+}
